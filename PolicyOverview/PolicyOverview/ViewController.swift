644b1fbd93a8d595edf5256bc0883d88ad3004bb
Macbook <rahulpatil281292@gmail.com>
2020-01-23 09:46:17 +0530

    B-65453: Added required objects

---
+++ PolicyOverview/PolicyOverview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PolicyOverview
+//
+//  Created by Macbook on 23/01/20.
+//  Copyright © 2020 com.rahulpatil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
