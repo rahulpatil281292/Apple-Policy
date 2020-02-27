ce7b188f5caaa1c8b8e48ef09e139658573f20c2
Rahul <rahulpatil281292@gmail.com>
2020-02-27 23:52:59 +0530

    Details-Added new viewcontroller and deleted main storyboard

---
+++ PolicyOverview/PolicyOverview/CustomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomViewController.swift
+//  PolicyOverview
+//
+//  Created by Macbook on 27/02/20.
+//  Copyright © 2020 com.rahulpatil. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
