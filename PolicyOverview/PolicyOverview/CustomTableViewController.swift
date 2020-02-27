02a72b1cbcb445ef05975c04c6e0260fd849365d
rahulpatil281292 <32728504+rahulpatil281292@users.noreply.github.com>
2020-02-27 10:53:40 +0530

    PolicyOverview-Updated files

--- PolicyOverview/PolicyOverview/CustomTableViewController.swift
+++ PolicyOverview/PolicyOverview/CustomTableViewController.swift
@@ -31,11 +31,11 @@ class CustomTableViewController: UITableViewController {
 
 extension CustomTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 22
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 22
     }
 }
 
